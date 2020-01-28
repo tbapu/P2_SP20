c8e8b0981639062c45ac4632347898b2bb8a6f5c
TashiB <tbapu@fwparker.org>
2020-01-28 13:29:23 -0600

    1-28-2020 Basic Imports Notes

---
+++ Notes/01_imports.py
@@ -0,0 +1,43 @@
+# Notes - Imports
+
+
+
+import math
+import random as r
+from math import *
+from import_me import settings
+from import_me.settings import *  # same thing as above
+import pandas as pd
+
+
+if __name__ == "__main__":
+    # this code only runs if you run THIS FILE
+    # import library directly
+    print(math.pi)
+
+    # import and assign shortcut name
+    print(r.randrange(1, 7))
+
+    # import everything using the wildcard *
+    print(pi)
+
+    # importing your own python files (modules)
+    print(settings.a)
+
+    # import with wildcard
+    print(a)
+
+
+    # using class/def from other files
+    print(double(double(a)))
+
+
+
+
+
+
+
+
+
+
+
---
+++ Notes/import_me/settings.py
@@ -0,0 +1,13 @@
+
+
+
+
+a = 5
+
+def double(x):
+    return x * 2
+
+
+
+if __name__ == "__main__":
+    print("a =", a)
